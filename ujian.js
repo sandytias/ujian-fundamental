e0cf9e51f2a860fd7a18dae9c968ae4dbc916982
sandytias <57651677+sandytias@users.noreply.github.com>
2019-11-18 12:24:56 +0700

    Add files via upload

---
+++ ujian.js
@@ -0,0 +1,41 @@
+// function lelang(menit){
+//     var harga = 10000
+//     for(var i=1; i<menit; i++){
+//         if(i%4 != 0){
+//             harga = harga * 1.20
+//             harga = Math.ceil(harga)
+//     }
+//     }else{
+//         harga = harga * 1.10
+//         harga = Math.ceil(harga)
+
+//     }
+// }
+// if(harga < 30000000){
+//     return `pada menit ke-${menit}. harga barang mencapai Rp.${harga.toLocaleString()}`
+// }else{
+//     return `pada menit ke-${menit}. barang sudah di beli`
+// }
+
+// console.log(lelang(49))
+
+
+
+
+
+
+function cekkoin(koin){
+    var total = 0
+    var k25 = 0
+    var k10 = 0
+    var k5 = 0
+    var k1 = 0
+    
+    while(koin>=0){
+    if(koin 25>=0){
+        koin=koin-25
+        total++
+        k25++
+    }else if(koin)
+
+    }
